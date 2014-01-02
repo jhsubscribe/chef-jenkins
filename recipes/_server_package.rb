1a563496928be35034ffafb1e7ec604b2977eae8
jhsubscribe <jhsubscribe@bigpond.com>
2014-01-02 15:52:01 +0000

    yum v3 changes

--- recipes/_server_package.rb
+++ recipes/_server_package.rb
@@ -33,14 +33,9 @@ when 'debian'
 when 'rhel'
   include_recipe 'yum'
 
-  yum_key 'RPM-GPG-KEY-jenkins-ci' do
-    url 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
-    action :add
-  end
-
   yum_repository 'jenkins-ci' do
-    url 'http://pkg.jenkins-ci.org/redhat'
-    key 'RPM-GPG-KEY-jenkins-ci'
+    baseurl 'http://pkg.jenkins-ci.org/redhat'
+    gpgkey 'http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key'
     action :add
   end
 
